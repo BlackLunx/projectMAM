ced2563558a5ab217b17c49b7b19a4033e54fca8
BlackLunx <mihonblack@gmail.com>
2019-06-02 23:24:24 +0300

    New Module

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+int main(){
+
+    RenderWindow window(VideoMode(480, 360), "SFML works!");
+    Image  firstImage;
+    firstImage.loadFromFile("Images/lol.jpg");
+
+    Texture firstTexture;
+    firstTexture.loadFromImage(firstImage);
+
+    Sprite firstSprite;
+    firstSprite.setTexture(firstTexture);
+    firstSprite.setTextureRect(IntRect(0, 10, 100, 100));
+    firstSprite.setPosition(50, 25);
+
+    while (window.isOpen()){
+        Event event;
+        while (window.pollEvent(event)){
+            if (event.type == Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(firstSprite);
+        window.display();
+    }
+
+    return 0;
+}
