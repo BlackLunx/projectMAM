4b022be8e9ca8f6dca8b69baf4bd2c1dfecf3479
BlackLunx <mihonblack@gmail.com>
2019-06-04 18:08:22 +0300

    Updated menu

--- menu.cpp
+++ menu.cpp
@@ -2,51 +2,74 @@
 // Created by JG on 03.06.2019.
 //
 #include <SFML/Graphics.hpp>
+#include "game.cpp"
 using namespace sf;
-void menu(Window &window){
-    Texture newGameTX, exitTX, backgroundTX, nameTX;
-    newGameTX.loadFromFile("Images/newGame.png");
-    exitTX.loadFromFile("Images/exit.png");
-    backgroundTX.loadFromFile("Images/background.png");
-    nameTX.loadFromFile("Images/name.png");
+void menu(RenderWindow &window){
 
-    Sprite newGameSP(newGameTX), exitSP(exitTX), backgroundSP(backgroundTX), nameSP(nameTX);
+    Texture newGame_TX1, newGame_TX2, exit_TX1, exit_TX2, background_TX, cursorTX;
+    newGame_TX1.loadFromFile("Images/newgamefirst.png");
+    newGame_TX2.loadFromFile("Images/newgamesecond.png");
+    exit_TX1.loadFromFile("Images/exitfirst.png");
+    exit_TX2.loadFromFile("Images/exitsecond.png");
+    background_TX.loadFromFile("Images/mainpicture.png");
+    cursorTX.loadFromFile("Images/cursor.png");
 
-    newGameSP.setPosition(100, 45);
-    exitSP.setPosition(100, 90);
-    backgroundSP.setPosition(345, 0);
-    nameSP.setPosition(100, 30);
+
+    Sprite newGameSP(newGame_TX1), newGameSPHL(newGame_TX2), exitSP(exit_TX1), exitSPHL(exit_TX2), backgroundSP(background_TX), cursorSP(cursorTX);
+
+    backgroundSP.setPosition(0, 0);
+    newGameSP.setPosition(800, 200);
+    exitSP.setPosition(800, 260);
+    newGameSPHL.setPosition(800, 200);
+    exitSPHL.setPosition(800, 260);
     int inMenu = 1;
     while(inMenu){
         int currentPosition = 0;
-        if(IntRect(100, 30, 300, 50).contains(Mouse::getPosition())){
-            currentPosition = 1;
+        window.clear(Color(129, 181, 221));
+        Event event;
+        while (window.pollEvent(event)){
+            if (event.type == Event::Closed){
+                window.close();
+                exit(0);
+            }
         }
-        if(IntRect(100, 90, 300, 50).contains(Mouse::getPosition())){
+        if(IntRect(800, 200, 950, 240).contains(Mouse::getPosition(window))){
+            currentPosition = 1;
+            cursorSP.setPosition(static_cast<Vector2f>(Mouse::getPosition(window)));
+        }
+        if(IntRect(800, 260, 950, 300).contains(Mouse::getPosition(window))){
             currentPosition = 2;
+            cursorSP.setPosition(static_cast<Vector2f>(Mouse::getPosition(window)));
         }
         if(Mouse::isButtonPressed(Mouse::Left)){
             if(currentPosition == 1){
                 inMenu = 0;
-                startGame(window);
+                window.close();
+                //startGame(window);
             }
             if(currentPosition == 2){
                 inMenu = 0;
                 window.close();
+
             }
         }
         window.clear();
         window.draw(backgroundSP);
-        window.draw(nameSP);
         if(currentPosition == 1){
-            window.draw(newGameSPHL)
+            window.setMouseCursorVisible(false);
+            window.draw(newGameSPHL);
+            window.draw(exitSP);
+            window.draw(cursorSP);
         }
         else{
             window.draw(newGameSP);
             if(currentPosition == 2){
-                window.draw(exitSPHL)
+                window.setMouseCursorVisible(false);
+                window.draw(exitSPHL);
+                window.draw(cursorSP);
             }
             else{
+                window.setMouseCursorVisible(true);
                 window.draw(exitSP);
             }
         }
