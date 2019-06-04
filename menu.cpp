1c6df8fdb9608c28de581824ec24e7b3ed037704
BlackLunx <mihonblack@gmail.com>
2019-06-04 18:57:06 +0300

    Added levels

--- menu.cpp
+++ menu.cpp
@@ -3,29 +3,35 @@
 //
 #include <SFML/Graphics.hpp>
 #include "game.cpp"
+#include <vector>
+#include <iostream>
 using namespace sf;
+
+void selectLevel(RenderWindow& window);
+
 void menu(RenderWindow &window){
 
     Texture newGame_TX1, newGame_TX2, exit_TX1, exit_TX2, background_TX, cursorTX;
     newGame_TX1.loadFromFile("Images/newgamefirst.png");
     newGame_TX2.loadFromFile("Images/newgamesecond.png");
+
     exit_TX1.loadFromFile("Images/exitfirst.png");
     exit_TX2.loadFromFile("Images/exitsecond.png");
+
     background_TX.loadFromFile("Images/mainpicture.png");
     cursorTX.loadFromFile("Images/cursor.png");
 
 
     Sprite newGameSP(newGame_TX1), newGameSPHL(newGame_TX2), exitSP(exit_TX1), exitSPHL(exit_TX2), backgroundSP(background_TX), cursorSP(cursorTX);
 
-    backgroundSP.setPosition(0, 0);
     newGameSP.setPosition(800, 200);
     exitSP.setPosition(800, 260);
     newGameSPHL.setPosition(800, 200);
     exitSPHL.setPosition(800, 260);
-    int inMenu = 1;
-    while(inMenu){
+
+    while(window.isOpen()){
         int currentPosition = 0;
-        window.clear(Color(129, 181, 221));
+
         Event event;
         while (window.pollEvent(event)){
             if (event.type == Event::Closed){
@@ -43,14 +49,11 @@ void menu(RenderWindow &window){
         }
         if(Mouse::isButtonPressed(Mouse::Left)){
             if(currentPosition == 1){
-                inMenu = 0;
-                window.close();
+                selectLevel(window);
                 //startGame(window);
             }
             if(currentPosition == 2){
-                inMenu = 0;
                 window.close();
-
             }
         }
         window.clear();
@@ -75,7 +78,78 @@ void menu(RenderWindow &window){
         }
         window.display();
     }
+}
+
+void selectLevel(RenderWindow& window){
+
+    Texture backgroundTX, cursorTX;
+    backgroundTX.loadFromFile("background.png");
+    cursorTX.loadFromFile("Images/cursor.png");
+
+    std::vector<Texture> textures(8);
+    textures[0].loadFromFile("Images/easyFirst.png");
+    textures[1].loadFromFile("Images/easySecond.png");
+
+    textures[2].loadFromFile("Images/mediumFirst.png");
+    textures[3].loadFromFile("Images/mediumSecond.png");
+
+    textures[4].loadFromFile("Images/hardFirst.png");
+    textures[5].loadFromFile("Images/hardSecond");
+
+    textures[6].loadFromFile("Images/backFirst");
+    textures[7].loadFromFile("Images/backSecond");
+
+    Sprite background(backgroundTX), cursorSP(cursorTX);
+
+    std::vector<std::pair<Sprite, Sprite> > draws(4);
+    for(int i = 0;i < 4;i++){
+        draws[i].first.setTexture(textures[i * 2]);
+        draws[i].second.setTexture(textures[i * 2 + 1]);
+        draws[i].first.setPosition(800, 200 + i * 60);
+        draws[i].second.setPosition(800, 200 + i * 60);
+    }
+
+    while(window.isOpen()){
 
 
+        Event event;
+        while (window.pollEvent(event)){
+            if (event.type == Event::Closed){
+                window.close();
+                exit(0);
+            }
+        }
+        std::vector<int> isHere(4, 0);
+
+
+        for(int i = 0;i < 4;i++){
+            if(IntRect(800, 200 + i * 60, 800, 200 + i * 100).contains(Mouse::getPosition(window))){
+                isHere[i] = 1;
+                cursorSP.setPosition(static_cast<Vector2f>(Mouse::getPosition(window)));
+            }
+        }
+        if(Mouse::isButtonPressed(Mouse::Left)){
+            for(int i = 0;i < 3;i++) if(isHere[i]){
+                //startGame(window, i);
+            }
+            if(isHere[3]){
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(background);
+        bool wasInButton = false;
+        for(int i = 0;i < 4;i++){
+            if(isHere[i]){
+                window.draw(draws[i].second);
+                wasInButton = 1;
+            }
+            else{
+                window.draw(draws[i].first);
+            }
+        }
+        window.setMouseCursorVisible(!wasInButton);
+        window.display();
+    }
 
 }
