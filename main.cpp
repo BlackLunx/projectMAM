e847999d2e321fe55047956646d1f75e1170b83f
BlackLunx <mihonblack@gmail.com>
2019-06-07 10:40:55 +0300

    Added levels

--- main.cpp
+++ main.cpp
@@ -1,29 +1,11 @@
 #include <SFML/Graphics.hpp>
-using namespace sf;
-int main(){
+#include "menu.cpp"
+#include "game.cpp"
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1190, 800), "SFML works!");
 
-    RenderWindow window(VideoMode(480, 360), "SFML works!");
-    Image  firstImage;
-    firstImage.loadFromFile("Images/lol.jpg");
 
-    Texture firstTexture;
-    firstTexture.loadFromImage(firstImage);
-
-    Sprite firstSprite;
-    firstSprite.setTexture(firstTexture);
-    firstSprite.setTextureRect(IntRect(0, 10, 100, 100));
-    firstSprite.setPosition(50, 25);
-вуцвуцвц
-    while (window.isOpen()){
-        Event event;
-        while (window.pollEvent(event)){
-            if (event.type == Event::Closed)
-                window.close();
-        }
-        window.clear();
-        window.draw(firstSprite);
-        window.display();
-    }
-
+    menu(window);
     return 0;
 }
